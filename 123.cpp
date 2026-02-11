004243b706cb915c94d679fd078f08dad16f104b
izzakhus <izzakhus@gmail.com>
2026-02-12 02:28:37 +0600

    HomeWork-1

---
+++ 123.cpp
@@ -0,0 +1,303 @@
+#include <iostream>
+#include <cstring>
+#include <cmath>
+
+using namespace std;
+int main(){
+    // Begin1
+    // int a;
+    // cin >> a;
+
+    // int P = 4 * a;
+    // cout << "Периметр " << P;
+
+    // Begin2
+    // int a;
+    // cin >> a;
+    // int S = a * a;
+    // cout << S;
+
+    // Begin3
+    // int a;
+    // cin >> a;
+    // int b;
+    // cin >> b;
+    
+    // int S = a * b;
+    // int P = 2*(a+b);
+    // cout << "Площадь " << S << endl << "Периметр " << P;
+
+    // Begin4
+    // int d;
+    // cin >> d; 
+    // float pi = 3.14;
+    // float L = pi * d;
+    // cout << L;
+    
+    //Begin5
+    // int a;
+    // cin >> a;
+    // int V = (a * a * a);
+    // int S = 6 * (a * a);
+    // cout << V << endl << S;
+
+    //Begin6
+    // int a;
+    // int b;
+    // int c;
+    // cin >> a;
+    // cin >> b;
+    // cin >> c;
+
+    // int V = a * b * c;
+    // int S = 2*(a * b + b * c + a * c);
+    // cout << V << endl << S;
+
+    //Begin7
+    // int r;
+    // cin >> r;
+    // float Pi = 3.14;
+    // float L = 2 * Pi * r;
+    // float S = Pi * (r*r);
+    // cout << L << endl << S;
+
+    //Begin8
+    // float a;
+    // cin >> a;
+    // float b;
+    // cin >> b;
+    // float S = (a+b) / 2;
+    // cout << S;
+
+    //Begin9
+    // int a;
+    // int b;
+    // cin >> a;
+    // cin >> b;
+    // int G = sqrt(a*b);
+    // cout << G;
+
+    //Begin10
+    // int a, b;
+    // cin >> a;
+    // cin >> b;
+    // int sum = a + b;
+    // int min = a - b;
+    // int mul = a * b;
+    // int sq =(a * a)/(b * b);
+    // cout << sum << endl << min << endl << mul<< endl << sq;
+
+    //integer1
+    // int L;
+    // cin >> L;
+    // cout << L / 100;
+    
+    // integer2
+    // int M;
+    // cin >> M;
+    // cout << M / 1000;
+
+    // integer3
+    // int bytes;
+    // cin >> bytes;
+    // cout << bytes / 1024;
+
+    //integer4
+    // int a;
+    // int b;
+    // cin >> a;
+    // cin >> b;
+    // cout << a / b;
+
+    //integer5
+    // int a;
+    // int b;
+    // cin >> a;
+    // cin >> b;
+    // cout << a % b;
+
+
+    //Boolean1,2,3
+    // int a;
+    // cin >> a;
+    // if (a>0){
+    //     cout << "Положительно";
+    // }else {
+    //     cout << "Отрицательно";
+    //     }   
+    // if (a % 2 != 0){
+    //     cout << "Нечетное";
+    // }else{
+    //     cout << "Четное";
+    // }
+
+    //Boolean4
+    // int a;
+    // int b;
+    // cin >> a;
+    // cin >> b;
+    // bool res = (a > 2) && (b <= 3);
+    // cout << res; 
+
+    //Boolean5
+    // int a;
+    // int b;
+    // cin >> a;
+    // cin >> b;
+    // bool res = (a >= 2) || (b < 2);
+    // cout << res; 
+    
+
+    //if1,2,3
+    // int a;
+    // cin >> a;
+    // if(a > 0){
+    //     a += 1;
+    // }else if (a < 0){
+    //     a -= 2;
+    // }else{
+    //     a = 10;
+    // }
+    // cout << a;
+
+    //if4
+    // int a;
+    // int b;
+    // int c;
+    // int d = 0;
+
+    // cin >>a;
+    // cin >>b;
+    // cin >>c;
+
+    // if(a >= 0){ d += 1;}
+    // if(b >= 0){ d += 1;}
+    // if(c >= 0){ d += 1;}
+    // cout << "Положительных чисел "<<  d;
+
+    //if5
+    // int a;
+    // int b;
+    // int c;
+    // int d = 0;
+    // int p = 0;
+    // cin >>a;
+    // cin >>b;
+    // cin >>c;
+
+    // if(a >= 0){ d += 1;}
+    // if(b >= 0){ d += 1;}
+    // if(c >= 0){ d += 1;}
+    // if(a <= 0){ p += 1;}
+    // if(b <= 0){ p += 1;}
+    // if(c <= 0){ p += 1;}
+    // cout << "Положительных чисел "<<  d << endl;
+    // cout << "Отрицательных чисел "<<  p;
+
+
+    //Case1
+    // int a;
+
+    // while (true){
+    //     cout << "Введите цифру от 1 до 7 ";
+    //     cin >> a; 
+    //     if(a >= 1 && a <= 7){
+    //         break;
+    //     }
+    //     cout << "Напишите число от 1 до 7 ";
+    // }
+    // switch (a) {
+    //     case 1: cout << "Понедельник"; 
+    //     break;
+    //     case 2: cout << "Вторник"; 
+    //     break;
+    //     case 3: cout << "Среда"; 
+    //     break;
+    //     case 4: cout << "Четверг"; 
+    //     break;
+    //     case 5: cout << "Пятница"; 
+    //     break;
+    //     case 6: cout << "Суббота";
+    //     break;
+    //     case 7: cout << "Воскресенье";
+    //     break;        
+    // }
+
+    //Case 2
+    // int a;
+    // while (true){
+    //     cout << "Напиши свою оценку от 1 до 5 ";
+    //     cin >> a; 
+    //     if(a >= 1 && a <= 5){
+    //         break;
+    //     }
+    //     cout << "Напишите число от 1 до 5 ";
+    // }
+    // switch (a) {
+    //     case 1: cout << "плохо";
+    //     break;
+    //     case 2: cout << "неудовлетворительно";
+    //     break;
+    //     case 3: cout << "удовлетворительно";
+    //     break;
+    //     case 4: cout << "хорошо";
+    //     break;
+    //     case 5: cout << "отлично";
+    //     break;
+    // }
+
+
+    //case3
+    // int a;
+    // while (true){
+    //     cout << "Напиши номер месяца от 1 до 12 ";
+    //     cin >> a; 
+    //     if(a >= 1 && a <= 12){
+    //         break;
+    //     }
+    //     cout << "Напишите число от 1 до 12 ";
+    // }
+    // switch (a) {
+    //     case 12: case  1: case  2:
+    //     cout << "зима";
+    //     break;
+
+    //     case  3: case  4: case  5:
+    //     cout << "весна";
+    //     break;
+
+    //     case  6: case  7: case  8:
+    //     cout << "лето";
+    //     break;
+
+    //     case  9: case 10: case 11:
+    //     cout << "осень";
+    //     break;
+    // }
+
+
+    int a;
+    int day;
+    while (true){
+        cout << "Напиши номер месяца от 1 до 12 ";
+        cin >> a; 
+        if(a >= 1 && a <= 12){
+            break;
+        }
+        cout << "Напишите число от 1 до 12 ";
+    }
+    switch (a) {
+        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+        day = 31;
+        break;
+
+        case 4: case 6: case 9: case 11:
+        day = 30;
+        break;
+
+        case 2:
+        day = 28;
+        break;
+    }
+    cout << "В этом месяце дней " << day;
+}
---
+++ task.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+int main(){
+    int a;
+    cin>> a;
+
+    int P = 4 * a;
+    cout << "периметр "
+}
