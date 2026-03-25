542f97fb30ce4b7172195cf95949ffee7213054b
izzakhus <izzakhus@gmail.com>
2026-03-26 02:04:47 +0600

    HomeWork-3

---
+++ massive.cpp
@@ -0,0 +1,152 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 1
+    // int n;
+    // cin >> n;
+    // int a[100];
+    // int s = 0;
+    // for (int i = 0; i < n; i++) {
+    //     cin >> a[i];
+    //     s = s + a[i];
+    // }
+    // cout << s << endl;
+
+    // 2
+    // int n;
+    // cin >> n;
+    // int a[100];
+    // for (int i = 0; i < n; i++) {
+    //     cin >> a[i];
+    // }
+    // int m = a[0];
+    // for (int i = 1; i < n; i++) {
+    //     if (a[i] > m) {
+    //         m = a[i];
+    //     }
+    // }
+    // cout << m << endl;
+
+
+    // 3
+    // int n;
+    // cin >> n;
+    // int a[100];
+    // int c = 0;
+    // for (int i = 0; i < n; i++) {
+    //     cin >> a[i];
+    //     if (a[i] % 2 == 0) {
+    //         c = c + 1;
+    //     }
+    // }
+    // cout << c << endl;
+
+    // 4
+    // int n;
+    // cin >> n;
+    // int a[100];
+    // double s = 0;
+    // for (int i = 0; i < n; i++) {
+    //     cin >> a[i];
+    //     s = s + a[i];
+    // }
+    // cout << "Srednee znachenie = " << s / n << endl;
+
+    // 5
+    // int n;
+    // cin >> n;
+    // int a[100];
+    // for (int i = 0; i < n; i++) {
+    //     cin >> a[i];
+    // }
+    // int m = a[0];
+    // for (int i = 1; i < n; i++) {
+    //     if (a[i] < m) {
+    //         m = a[i];
+    //     }
+    // }
+    // cout << m << endl;
+
+    // 6
+    // int n;
+    // cin >> n;
+    // int a[100];
+    // int s = 0;
+    // for (int i = 0; i < n; i++) {
+    //     cin >> a[i];
+    //     if (a[i] > 0) {
+    //         s = s + a[i];
+    //     }
+    // }
+    // cout << s << endl;
+
+    // 7
+    // int n;
+    // cin >> n;
+    // int a[100];
+    // for (int i = 0; i < n; i++) {
+    //     cin >> a[i];
+    //     if (a[i] < 0) {
+    //         a[i] = 0;
+    //     }
+    // }
+    // for (int i = 0; i < n; i++) {
+    //     cout << a[i] << " ";
+    // }
+    // cout << endl;
+
+    // 8
+    // int n;
+    // cin >> n;
+    // int a[100];
+    // double s = 0;
+    // for (int i = 0; i < n; i++) {
+    //     cin >> a[i];
+    //     s = s + a[i];
+    // }
+    // double avg = s / n;
+    // int c = 0;
+    // for (int i = 0; i < n; i++) {
+    //     if (a[i] > avg) {
+    //         c = c + 1;
+    //     }
+    // }
+    // cout << c << endl;
+
+
+    // 9
+    // int n;
+    // cin >> n;
+    // int a[100];
+    // for (int i = 0; i < n; i++) {
+    //     cin >> a[i];
+    // }
+    // int t = a[0];
+    // a[0] = a[n - 1];
+    // a[n - 1] = t;
+    // for (int i = 0; i < n; i++) {
+    //     cout << a[i] << " ";
+    // }
+    // cout << endl;
+
+    // 10
+    //     int n, x;
+    //     cin >> n >> x;
+    //     int a[100];
+    //     int f = 0;
+    //     for (int i = 0; i < n; i++) {
+    //         cin >> a[i];
+    //         if (a[i] == x) {
+    //             f = 1;
+    //         }
+    //     }
+    //     if (f == 1) {
+    //         cout << "YES" << endl;
+    //     } else {
+    //         cout << "NO" << endl;
+    //     }
+    
+    // return 0;
+}
